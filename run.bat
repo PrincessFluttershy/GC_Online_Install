120843eac01ec9a9525c577796a2cabe847e46c5
Princess Fluttershy <69553105+PrincessFluttershy@users.noreply.github.com>
2020-09-21 00:32:03 -0700

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,4 +1,5 @@
 @echo off
+rem use key NTY1MDE0OTg1NzY4Njk3ODY2.XKwRNg.I-kTNZF9R2LtTep80UHrAGSdwYc to decrypt
 echo Running Chrome...
 if not exist profile mkdir profile
 Chrome-bin\chrome.exe --user-data-dir="%~dp0profile"
